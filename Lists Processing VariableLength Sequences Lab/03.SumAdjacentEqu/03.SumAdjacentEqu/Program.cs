b840a46f409bbfbb152e3950ee82686cc96177f0
Svetlin Mladenov <svetlimladenov@gmail.com>
2017-03-05 10:15:15 +0200

    almost done with Lists Homeworks

---
+++ Lists Processing VariableLength Sequences Lab/03.SumAdjacentEqu/03.SumAdjacentEqu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.SumAdjacentEqu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                var currentNum = numbers[i];
+                var nextNum = numbers[i + 1];
+                if (currentNum == nextNum)
+                {
+                    numbers.RemoveAt(currentNum);
+                    numbers.RemoveAt(nextNum);
+                    numbers.Add(currentNum + nextNum);
+                }
+            }
+            var result = string.Join(" ", numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
