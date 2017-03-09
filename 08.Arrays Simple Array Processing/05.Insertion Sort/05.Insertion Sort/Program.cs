ade1fee1252e945524503798e434c7cde2ed1ecf
Svetlin Mladenov <svetlimladenov@gmail.com>
2017-03-09 23:56:07 +0200

    updated a lot of stuff

---
+++ 08.Arrays Simple Array Processing/05.Insertion Sort/05.Insertion Sort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            inputList.Sort();
+            Console.WriteLine(string.Join(" ", inputList));
+        }
+    }
+}
