4857f5c0aa54eeec0b63fed319f50b84ddea8551
Svetlin Mladenov <svetlimladenov@gmail.com>
2017-03-19 23:45:01 +0200

    Started Dictionaries

--- 08.Arrays Simple Array Processing/05.Insertion Sort/05.Insertion Sort/Program.cs
+++ 08.Arrays Simple Array Processing/05.Insertion Sort/05.Insertion Sort/Program.cs
@@ -10,6 +10,7 @@ namespace _05.Insertion_Sort
     {
         static void Main(string[] args)
         {
+            //bcs i am lazy person :D but i am writing a comment ;(
             var inputList = Console.ReadLine()
                 .Split(' ')
                 .Select(int.Parse)
