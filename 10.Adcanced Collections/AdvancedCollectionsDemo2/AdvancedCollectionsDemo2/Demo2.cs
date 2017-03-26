62f2df1fbd69bdfee56450a5f0df254700d3b447
Svetlin Mladenov <svetlimladenov@gmail.com>
2017-03-26 18:20:14 +0300

    started Advanced Collections

---
+++ 10.Adcanced Collections/AdvancedCollectionsDemo2/AdvancedCollectionsDemo2/Demo2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCollectionsDemo2
+{
+    class Demo2
+    {
+        public static void Main()
+        {
+            var input = int.Parse(Console.ReadLine());
+        }
+    }
+}
