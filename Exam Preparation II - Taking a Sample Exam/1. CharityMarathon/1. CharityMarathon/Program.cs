30b09cd7e5d42fbaaff258e76c377daea488d026
Svetlin Mladenov <svetlimladenov@gmail.com>
2017-04-18 15:20:09 +0300

    done with 1st,2nd and 3rd problems from Exam Preparation II

--- Exam Preparation II - Taking a Sample Exam/1. CharityMarathon/1. CharityMarathon/Program.cs
+++ Exam Preparation II - Taking a Sample Exam/1. CharityMarathon/1. CharityMarathon/Program.cs
@@ -8,8 +8,25 @@ namespace _1.CharityMarathon
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var marathonDays = long.Parse(Console.ReadLine());
+            var runners = long.Parse(Console.ReadLine());
+            var avgNumLaps = long.Parse(Console.ReadLine());
+            var trackLenght = long.Parse(Console.ReadLine());
+            var trackCapacity = long.Parse(Console.ReadLine());
+            var moneyPerKm = decimal.Parse(Console.ReadLine());
+
+            var maxRunners = marathonDays * trackCapacity;
+            var willRun = runners;
+            if (runners > maxRunners)
+            {
+                willRun = maxRunners;
+            }
+            var totalKM = (willRun * avgNumLaps * trackLenght) / 1000M;
+            var moneyRaised = totalKM * moneyPerKm;
+
+            Console.WriteLine("Money raised: {0:F2}",moneyRaised);
         }
     }
 }
