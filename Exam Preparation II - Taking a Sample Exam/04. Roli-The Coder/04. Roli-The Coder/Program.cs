3122d7d1c5526435b2df25dee514cdb5abf99933
Svetlin Mladenov <svetlimladenov@gmail.com>
2017-04-20 21:54:47 +0300

    doing old exams

---
+++ Exam Preparation II - Taking a Sample Exam/04. Roli-The Coder/04. Roli-The Coder/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Roli_The_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currenEventName = new List<string>();
+            var result = new SortedDictionary<int, SortedDictionary<string, SortedSet<string>>>();
+
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "Time for Code")
+                {
+                    break;
+                }
+                var tokens = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var id = int.Parse(tokens[0]);
+                var eventName = tokens[1];
+                var participantsOfEvent = tokens.Skip(2).ToArray();
+
+                if (!result.ContainsKey(id))
+                {
+                    result[id] = new SortedDictionary<string, SortedSet<string>>();
+                }
+
+
+                if (!result[id].ContainsKey(eventName) && eventName[0] == '#')
+                {       
+                        result[id][eventName] = new SortedSet<string>();
+                                 
+                }
+
+                for (int i = 0; i < participantsOfEvent.Length; i++)
+                {
+                    result[id][eventName].Add(participantsOfEvent[i]);
+
+                }
+
+
+            }  
+            foreach (var kvp in result)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var kur in kvp.Value)
+                {
+                    Console.WriteLine( kur.Key + " - " + kur.Value.Count);
+                    foreach (var asdf in kur.Value)
+                    {
+                        Console.WriteLine(asdf);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Exam Preparation III/02. Command Interpreter/02. Command Interpreter/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Command_Interpreter
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var collection = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (true)
+            {
+                var command = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (command[0] == "end")
+                {
+                    break;
+                }
+
+                if (command[0] == "reverse")
+                {
+                    var reverseFrom = int.Parse(command[2]);
+                    var count = int.Parse(command[4]);
+                    var result = collection
+                        .Skip(reverseFrom)
+                        .Take(count)
+                        .Reverse()
+                        .ToArray();
+                    var firstPart = collection.Take(reverseFrom);
+                    var secondPart = collection.Reverse().Take(collection.Length - count - reverseFrom).Reverse();
+                    Console.WriteLine("["
+                        + string.Join(", ", firstPart)
+                        + ", "
+                        + string.Join(", ", result)
+                        + ", "
+                        + string.Join(", ", secondPart)
+                        + "]");
+
+                }
+                else if (command[0] == "sort")
+                {
+                    var reverseFrom = int.Parse(command[2]);
+                    var count = int.Parse(command[4]);
+                    var result = collection
+                        .Skip(reverseFrom)
+                        .Take(count)
+                        .OrderBy(a => a)
+                        .ToArray();
+                    var firstPart = collection.Take(reverseFrom);
+                    var secondPart = collection.Reverse().Take(collection.Length - count - reverseFrom).Reverse();
+                    if (firstPart.Count() > 0)
+                    {
+                        Console.WriteLine("["
+                            + string.Join(", ", firstPart)
+                            + ", "
+                            + string.Join(", ", result)
+                            + ", "
+                            + string.Join(", ", secondPart)
+                            + "]");
+                    }
+                    else
+                    {
+                        Console.WriteLine("["
+                            + string.Join(", ", result)
+                            + ", "
+                            + string.Join(", ", secondPart)
+                            + "]");
+                    }
+                }
+                else if (command[0] == "rollLeft")
+                {
+
+                }
+                else if (command[0] == "rollRight")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                }
+            }
+
+        }
+    }
+}
