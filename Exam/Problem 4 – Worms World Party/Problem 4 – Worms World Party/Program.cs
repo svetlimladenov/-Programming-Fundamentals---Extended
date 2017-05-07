3b3d82fae92513fccf4d8a45004259e7199ed725
Svetli Mladenov <svetlimladenov@gmai.com>
2017-05-08 01:04:56 +0300

    preparing for the exam

---
+++ Exam/Problem 4 – Worms World Party/Problem 4 – Worms World Party/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4___Worms_World_Party
+{
+    class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+                if (inputLine == "quit")
+                {
+                    break;
+                }
+                var inputParams = inputLine.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine();    
+            }
+            Console.WriteLine();
+        }
+    }
+}
